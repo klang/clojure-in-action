c009f269c633c082305708a71789029043774771
Karsten Lang <k@lang.dk>
2012-06-15 13:43:11 +0200

    dynamic html for adding field sets in javascript

---
+++ mysql/public/dynamic-add-fields.js
@@ -0,0 +1,32 @@
+
+// http://www.satya-weblog.com/2010/02/add-input-fields-dynamically-to-form-using-javascript.html
+
+var ct = 1;
+
+function new_link()
+{
+	ct++;
+	var div1 = document.createElement('div');
+	div1.id = ct;
+
+	// link to delete extended form elements
+	var delLink = '<div style="text-align:right;margin-right:65px"><a href="javascript:delIt('+ ct +')">Del</a></div>';
+
+	div1.innerHTML = document.getElementById('newlinktpl').innerHTML + delLink;
+
+	document.getElementById('newlink').appendChild(div1);
+
+}
+// function to delete the newly added set of elements
+function delIt(eleId)
+{
+	d = document;
+
+	var ele = d.getElementById(eleId);
+
+	var parentEle = d.getElementById('newlink');
+
+	parentEle.removeChild(ele);
+
+}
+
